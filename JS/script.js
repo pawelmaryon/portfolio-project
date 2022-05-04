1a205a5fd44bf1e33147f341d6d20c81591b977f
P Maryon <maryonpawel@gmail.com>
2022-05-04 10:26:21 +0100

    button menu on mobile done

--- JS/script.js
+++ JS/script.js
@@ -1,13 +1,26 @@
+const bodyHomePage = document.querySelector(".home-page")
+
+
 const mobileMenue = document.querySelector(".mobile-menu")
 const mainMenu = document.querySelector(".main-menu")
 const childPage = document.querySelector(".child-page")
 const appWrapper = document.querySelector(".app-wrapper")
-mobileMenue.addEventListener("click", () => {
-  mainMenu.classList.remove("hidden")
-  mobileMenue.classList.add("hidden")
-})
-appWrapper.addEventListener("click", () => {
-  console.log("clicked");
+
+let width = screen.width
+console.log(width);
+if (width < 600) {
+  mobileMenue.addEventListener("click", () => {
+    mainMenu.classList.remove("hidden")
+    mobileMenue.classList.add("hidden")
+  })
+  appWrapper.addEventListener("click", () => {
+    console.log("clicked");
     mainMenu.classList.add("hidden")
     mobileMenue.classList.remove("hidden")    
-})
+  })
+  mainMenu.classList.add("hidden")
+  mobileMenue.classList.remove("hidden")    
+} else {
+  mainMenu.classList.remove("hidden")
+  mobileMenue.classList.add("hidden")
+}

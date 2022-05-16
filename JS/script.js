02f151dc77b63e8dca6a21b72def6e53254f9c45
P Maryon <maryonpawel@gmail.com>
2022-05-16 12:15:59 +0100

    mobile responsiveness done

--- JS/script.js
+++ JS/script.js
@@ -6,12 +6,22 @@ const mainMenu = document.querySelector(".main-menu")
 const childPage = document.querySelector(".child-page")
 const appWrapper = document.querySelector(".app-wrapper")
 
+const allLinkTags = document.querySelectorAll(".navigation-links")
+
+console.log(allLinkTags);
+
 let width = screen.width
 console.log(width);
 if (width < 600) {
+  for (i=0 ; i < allLinkTags.length ; i++) {
+    allLinkTags[i].addEventListener("click", () => {
+      mainMenu.classList.add("hidden")
+      mobileMenue.classList.remove("hidden") 
+    })
+  }
   mobileMenue.addEventListener("click", () => {
     mainMenu.classList.remove("hidden")
-    mobileMenue.classList.add("hidden")
+    mobileMenue.classList.add("hidden") 
   })
   appWrapper.addEventListener("click", () => {
     console.log("clicked");
